dc2b9962ab6d770308ce58f40fe2e352336a8bbf
ilya <ilya@ilya.ilya.org>
2026-06-02 21:29:47 +0300

    добавил erase в тесты

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,6 +1,4 @@
 #!/usr/bin/env bash
-set -euo pipefail
-
 DISK=${1:?Usage: scripts/test.sh /dev/loopX [mountpoint]}
 MNT=${2:-/mnt}
 
@@ -13,4 +11,12 @@ sudo userspace/myfsctl mapping "$MNT" file_000000
 sudo userspace/myfsctl hashes "$MNT" | head
 sudo userspace/myfsctl zero "$MNT"
 sudo userspace/myfsctl test "$MNT"
+sudo userspace/myfsctl erase "$MNT"
 sudo umount "$MNT"
+sudo mount -t myfs "$DISK" "$MNT"
+sudo userspace/myfsctl test "$MNT"
+echo "erase успешно отработал"
+
+sudo umount "$MNT"
+
+
